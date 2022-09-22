65eb6d2e83a7a8ce7470eff4494f55a55057252f
oezingle <oezingle@gmail.com>
2022-09-22 01:15:37 -0700

    Add auth test

--- README.md
+++ README.md
@@ -19,5 +19,31 @@ Copy the example env vars file
 ```sh
 cp .example.env .env
 ```
+Open `.env` in your favorite text editor and be ready to input the keys we're about to get from twitter.
+ - Go the [Twitter Developer Portal](https://developer.twitter.com/en/portal/dashboard)
+ - Click "Projects and Apps" (left pane)
+ - Click "Add App"
+    - Set the Environment to "Production"
+    - Give it a snazzy name
+    - Copy the "API Key" to TWITTER_BOT_APP_KEY in `.env`
+    - Copy the "API Key Secret" to TWITTER_BOT_APP_SECRET in `.env`
+ - Click "App Settings"
+ - In "User Authentication Settings" (Below "App Details"), click "Set up"
+    - Set the App Permissions to "Read and write"
+    - Set the Type of App to "Web App, Automated App, or Bot"
+    - Set the Callback URI to "https://localhost" (we don't use this)
+    - Set the Website URL to your own twitter, or your blog, or anything really
+    - Click "Save"
 
-Open `.env` in your favorite text editor
+Now, the Twitter setup is done. You just have to build the project, and log in.
+```bash
+npm run test auth # promts you to log in if you need to
+```
+
+### Running
+Running the bot is easy! Just `cd` to the project directory and run `node build/index.js`. Photos in `images/unposted/` will be posted by the bot
+
+## Caveats
+ - Only pictures
+ - Pictures saved with the wrong filetype won't work (eg, JPEG file called "image.png")
+ - The bot skips posts while there aren't any images left to post
--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,21 @@
 
 import getConfig from "./getConfig"
 import { postRandomImage } from "./getImage"
+import getTwitter from "./getTwitter"
 import { postText } from "./post"
 
 const main = async () => {
     const operator = process.argv[2].toLowerCase()
 
     switch (operator) {
+        case "auth": {
+            console.log("Logging you in if you need to")
+
+            await getTwitter()
+
+            break
+        }
+
         case "config": {
             console.log("Printing config")
 
@@ -18,7 +27,7 @@ const main = async () => {
         case "post": {
             console.log("Posting a random image from images/unposted")
 
-            postRandomImage()
+            await postRandomImage()
 
             break
         }
@@ -28,18 +37,19 @@ const main = async () => {
 
             console.log(`Posting the text '${str}'`)
 
-            postText(str)
+            await postText(str)
 
             break
         }
 
         default: {
             console.log("Use a valid switch:")
-            console.log("npm run test <config|post|text>")
+            console.log("npm run test <auth|config|post|text>")
 
+            console.log(" - auth: log into twitter if tokens.json doesn't exist")
+            console.log(" - config: print the configuration as parsed from the .env file")
             console.log(" - post: post a random image as the bot will every 24 hours")
             console.log(" - text: post the text 'hello twitter :)'")
-            console.log(" - config: print the configuration as parsed from the .env file")
 
             break
         }
